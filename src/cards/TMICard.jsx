b40f403a48aaadbf210e38faaccf312bb5b43ff3
dolphin2410 <80996692+dolphin2410@users.noreply.github.com>
2023-12-09 22:04:00 +0900

    Update TMICard.jsx

--- src/cards/TMICard.jsx
+++ src/cards/TMICard.jsx
@@ -5,7 +5,6 @@ export default function TMICard() {
     return (
         <Card helpers={{bottom: null, right: null, left: null, top: "RETURN"}}>
             - 생일 1월 24일<br/>
-            - 일본어 공부중<br/>
             - 샤인머스켓 좋아함<br />
             - 마인크래프트 좋아함<br />
             - 수학 좋아함<br />
